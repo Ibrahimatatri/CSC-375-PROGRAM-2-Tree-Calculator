30ad283f074eb7365085bf046846a0edd1906009
Ibrahimatatri <ibrahimatatri@gmail.com>
2026-03-20 10:56:54 -0400

    Add Node class with basic functionality
    Implement Node class with data and child pointers

--- Node.cpp
+++ Node.cpp
@@ -1,1 +1,45 @@
+#include "Node.h"
 
+Node::Node()
+{
+    data = "";
+    left = nullptr;
+    right = nullptr;
+}
+
+Node::Node(string value)
+{
+    data = value;
+    left = nullptr;
+    right = nullptr;
+}
+
+void Node::setData(string value)
+{
+    data = value;
+}
+
+void Node::setLeft(Node* ptr)
+{
+    left = ptr;
+}
+
+void Node::setRight(Node* ptr)
+{
+    right = ptr;
+}
+
+string Node::getData() const
+{
+    return data;
+}
+
+Node* Node::getLeft() const
+{
+    return left;
+}
+
+Node* Node::getRight() const
+{
+    return right;
+}
