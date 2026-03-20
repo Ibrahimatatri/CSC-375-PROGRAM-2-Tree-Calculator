d3c78f3b2f15eed70d9bbdf6efc14ed2e49d7ab5
Ibrahimatatri <ibrahimatatri@gmail.com>
2026-03-20 10:56:29 -0400

    Add Node class definition with basic methods

--- Node.h
+++ Node.h
@@ -1,1 +1,27 @@
+#ifndef NODE_H
+#define NODE_H
 
+#include <string>
+using namespace std;
+
+class Node
+{
+private:
+    string data;   // stores number or operator
+    Node* left;    // left child
+    Node* right;   // right child
+
+public:
+    Node();
+    Node(string value);
+
+    void setData(string value);
+    void setLeft(Node* ptr);
+    void setRight(Node* ptr);
+
+    string getData() const;
+    Node* getLeft() const;
+    Node* getRight() const;
+};
+
+#endif
