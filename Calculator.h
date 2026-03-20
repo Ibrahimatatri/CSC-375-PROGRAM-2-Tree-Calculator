2fff2ffc47d56343cd840b242b3ba478639a3cde
Ibrahimatatri <ibrahimatatri@gmail.com>
2026-03-20 10:58:28 -0400

    Add Calculator class with expression evaluation methods

--- Calculator.h
+++ Calculator.h
@@ -1,1 +1,24 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
 
+#include <string>
+#include "BinaryTree.h"
+#include "Stack.h"
+using namespace std;
+
+class Calculator
+{
+private:
+    bool isOperator(string token) const;
+    bool isDigitChar(char ch) const;
+    int precedence(string op) const;
+    string trimTrailingZeros(double value) const;
+
+    void processOperator(Stack<Node*>& values, Stack<string>& ops);
+    BinaryTree* buildExpressionTree(string expression);
+
+public:
+    void run(); // main loop
+};
+
+#endif
